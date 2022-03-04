b6f8e532a5c5a63dbe3f347c425611875f5445e8
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-03-04 12:27:20 +0900

    Update variables.auto.tf

--- variables.auto.tf
+++ variables.auto.tf
@@ -10,17 +10,10 @@ variable "server_list" {
   description = "Servers (identified by name, object_type, and target_platform) to assign to configured server profiles"
   default     =  [
     {
-      name                   = "FI6454-1-3",
+      name                   = "UCSX-FI-1-2",
       object_type            = "compute.Blade",
       target_platform        = "FIAttached",
       vmedia_policy          = "NFS-3",
-      boot_iso_file_location = "172.168.18.10/esxi.iso"
-    },
-    {
-      name                   = "FI6454-1-4",
-      object_type            = "compute.Blade",
-      target_platform        = "FIAttached",
-      vmedia_policy          = "NFS-4",
       boot_iso_file_location = "172.168.18.10/esxi.iso"
     }
   ]
